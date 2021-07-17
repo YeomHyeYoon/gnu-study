a1b34681c1fb43c7208cdfdf234733318ed28dd9
Yuni <hyeyoon5019@naver.com>
2021-07-17 15:31:12 +0900

    modify source

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import React,{useState , useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [user,setUser] = useState("");
+
+ useEffect(()=>{
+  Axios.get("/api/users")
+  .then(function(response){
+    if(response.data){
+      setUser(response.data);
+    }else{
+      alert("failed to");
+    }
+  }); 
+ },[]);
+
+ return(
+   <div className="App">
+     <header className="App-header">
+      <h1>{user.id}</h1>
+      <h1>{user.username}</h1>
+      <h1>{user.password}</h1>
+      <h1>{user.email}</h1>
+     </header>
+   </div>
+ );
 }
 
 export default App;
